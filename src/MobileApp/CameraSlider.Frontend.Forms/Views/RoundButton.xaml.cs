f239bcc1f51c2597efa8fd5fdc337343f46176d7
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2017-03-19 22:07:19 +0100

    Add MobileApp project

---
+++ src/MobileApp/CameraSlider.Frontend.Forms/Views/RoundButton.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CameraSlider.Frontend.Forms.Views
+{
+    public partial class RoundButton : ContentView
+    {
+        public RoundButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
