6fb1056be29c1d73cb51896b64c8eebcee026d87
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2017-04-02 16:01:41 +0200

    Handle Bluetooth Device clicked

--- src/MobileApp/CameraSlider.Frontend.Forms/Pages/DeviceSelectionPage.xaml.cs
+++ src/MobileApp/CameraSlider.Frontend.Forms/Pages/DeviceSelectionPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 using Xamarin.Forms;
 using CameraSlider.Frontend.Shared.ViewModels;
+using CameraSlider.Frontend.Shared.Models;
 
 namespace CameraSlider.Frontend.Forms.Pages
 {
@@ -21,6 +22,16 @@ namespace CameraSlider.Frontend.Forms.Pages
         {
             base.OnAppearing();
             await viewModel.RefreshAsync();
+        }
+
+        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var bluetoothDevice = e.SelectedItem as IBluetoothDevice;
+            if (bluetoothDevice != null)
+                viewModel.ConnectToDeviceCommand.Execute(bluetoothDevice);
+
+            // Reset selection
+            (sender as ListView).SelectedItem = null;
         }
     }
 }
