35ce48541630db14ad87ea8f3a8dcc118f15d22c
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2017-07-27 21:01:53 -0600

    Add provisional logo

--- src/MobileApp/CameraSlider.Frontend.Forms.Android/MainActivity.cs
+++ src/MobileApp/CameraSlider.Frontend.Forms.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace CameraSlider.Frontend.Forms.Droid
 {
-    [Activity(Label = "CameraSlider.Frontend.Forms", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Camera Slider", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
