ee1cdc0702ca0c4144c5fa4864273ea14da1b04f
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2017-07-27 20:41:09 -0600

    Add Configuration page

--- src/MobileApp/CameraSlider.Frontend.Forms/App.xaml.cs
+++ src/MobileApp/CameraSlider.Frontend.Forms/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CameraSlider.Frontend.Forms.Pages;
 
 using Xamarin.Forms;
 
---
+++ src/MobileApp/CameraSlider.Frontend.Forms/Pages/ConfigurationPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using CameraSlider.Frontend.Shared.ViewModels;
+
+namespace CameraSlider.Frontend.Forms.Pages
+{
+    public partial class ConfigurationPage : ContentPage
+    {
+        private ConfigurationViewModel viewModel;
+
+        public ConfigurationPage()
+        {
+            InitializeComponent();
+            BindingContext = viewModel = App.ServiceLocator.ConfigurationViewModel;
+        }
+    }
+}
