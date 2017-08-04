313d948248771055b45c852739d65f70ba27e4bc
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2017-08-04 14:52:47 +0200

    Add Mobile Center integration

--- src/MobileApp/CameraSlider.Frontend.Forms/App.xaml.cs
+++ src/MobileApp/CameraSlider.Frontend.Forms/App.xaml.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using CameraSlider.Frontend.Forms.Pages;
-
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+using Microsoft.Azure.Mobile.Distribute;
 using Xamarin.Forms;
 
 [assembly: Xamarin.Forms.Xaml.XamlCompilation(Xamarin.Forms.Xaml.XamlCompilationOptions.Compile)]
@@ -36,7 +39,11 @@ namespace CameraSlider.Frontend.Forms
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+            MobileCenter.Start(
+                "android=f7090947-7e5b-4e61-9adf-4003961fa537;",
+                typeof(Analytics),
+                typeof(Crashes),
+                typeof(Distribute));
         }
 
         protected override void OnSleep()
