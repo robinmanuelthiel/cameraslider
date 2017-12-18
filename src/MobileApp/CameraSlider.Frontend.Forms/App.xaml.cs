163216f2e099cd56e8ef4023b3439943d2d13566
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2017-12-18 15:20:55 +0100

    Add App Center Push for iOS

--- src/MobileApp/CameraSlider.Frontend.Forms/App.xaml.cs
+++ src/MobileApp/CameraSlider.Frontend.Forms/App.xaml.cs
@@ -14,6 +14,9 @@ using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
 using Microsoft.AppCenter.Distribute;
+using Microsoft.AppCenter.Push;
+using GalaSoft.MvvmLight.Ioc;
+using CameraSlider.Frontend.Shared.Services;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace CameraSlider.Frontend.Forms
@@ -53,15 +56,26 @@ namespace CameraSlider.Frontend.Forms
 
             // Enable Mobile Center only if not running on Simulator/Emulator or in Debug Mode
             var environment = DependencyService.Get<IEnvironmentService>();
-            if (environment?.IsRunningInRealWorld() == true)
+            if (environment?.IsRunningInRealWorld() == false)
             {
-                AppCenter.Start(
-                    "ios=177833a8-ea1c-4ceb-b784-83330ca933b7;" +
-                    "android=0036cb00-df40-4131-a919-6e5a83b0371c;",
-                    typeof(Analytics),
-                    typeof(Crashes),
-                    typeof(Distribute));
+                Analytics.SetEnabledAsync(false);
+                Crashes.SetEnabledAsync(false);
+                Distribute.SetEnabledAsync(false);
             }
+
+            AppCenter.Start(
+                "ios=177833a8-ea1c-4ceb-b784-83330ca933b7;" +
+                "android=0036cb00-df40-4131-a919-6e5a83b0371c;",
+                typeof(Analytics),
+                typeof(Crashes),
+                typeof(Distribute),
+                typeof(Push));
+
+            Push.PushNotificationReceived += async (object sender, PushNotificationReceivedEventArgs e) =>
+            {
+                var dialogService = SimpleIoc.Default.GetInstance<IDialogService>();
+                await dialogService?.DisplayDialogAsync(e.Title, e.Message, "Ok");
+            };
         }
 
         protected override void OnSleep()
