1612aa85c94d08e0aceea6c8fb86b09c4e544bf8
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-01-03 12:37:07 +0100

    Adjusts listview selection background color on iOS

---
+++ src/MobileApp/CameraSlider.Frontend.Forms.iOS/CustomRenderers/ViewCellRendereriOS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CameraSlider.Frontend.Forms.iOS.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using UIKit;
+
+[assembly: ExportRenderer(typeof(ViewCell), typeof(ViewCellRendereriOS))]
+namespace CameraSlider.Frontend.Forms.iOS.CustomRenderers
+{
+    public class ViewCellRendereriOS : ViewCellRenderer
+    {
+        private UIView selectedBackground;
+
+        public override UIKit.UITableViewCell GetCell(Cell item, UIKit.UITableViewCell reusableCell, UIKit.UITableView tv)
+        {
+            var cell = base.GetCell(item, reusableCell, tv);
+
+            if (selectedBackground == null)
+            {
+                selectedBackground = new UIView(cell.SelectedBackgroundView.Bounds);
+                selectedBackground.Layer.BackgroundColor = Color.FromHex("#444444").ToCGColor();
+            }
+
+            cell.SelectedBackgroundView = selectedBackground;
+
+            return cell;
+        }
+    }
+}
